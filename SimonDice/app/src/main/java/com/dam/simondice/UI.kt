cbf9de87c52b87654ce7d945f30fded9ae3b76f2
dam <jdurancruz@danielcastelao.org>
2025-10-24 11:23:48 +0200

    Final del fin

--- SimonDice/app/src/main/java/com/dam/simondice/UI.kt
+++ SimonDice/app/src/main/java/com/dam/simondice/UI.kt
@@ -14,8 +14,8 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
